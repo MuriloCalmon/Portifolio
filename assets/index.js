a13c1bc28c7099b631a8710f99b24bbfe87a6f7c
Murilo Calmon <murilocalmon80@gmail.com>
2022-07-08 22:02:08 -0300

    feat: update code - clean code

--- assets/index.js
+++ assets/index.js
@@ -1,32 +1,28 @@
-/* 
-const saida = document.querySelector(".meu-nome");
+window.addEventListener('scroll', onScroll)
+onScroll()
 
-function digitacao(texto, contador) {
-  
-
-  if (contador < texto.length) {
-    setTimeout(() => {
-      saida.textContent += texto.charAt(contador);
-      contador++;
-      digitacao(texto, contador);
-    }, 200);
-  }
+function onScroll() {
+  addButtonBackToTopOnScroll()
 }
 
-digitacao("Murilo Calmon", 0); */
 
+var typed = new Typed('.dev-text-anim', {
+  strings: ['Desenvolvedor', 'Front-End', 'Programador'],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+})
 
-var typed = new Typed(".dev-text-anim", {
-  strings:["Desenvolvedor","Front-End", "Programador",],
-  typeSpeed:100,
-  backSpeed:60,
-  loop:true
-});
+var typed = new Typed('.meu-nome', {
+  strings: ['Murilo Calmon'],
+  typeSpeed: 100,
+  showCursor: false
+})
 
-var typed = new Typed(".meu-nome", {
-  strings:["Murilo Calmon"],
-  typeSpeed:100,
-  showCursor: false,
-});
-
-
+function addButtonBackToTopOnScroll() {
+  if (scrollY > 400) {
+    btnBack.classList.add('back-to-top')
+  } else {
+    btnBack.classList.remove('back-to-top')
+  }
+}
